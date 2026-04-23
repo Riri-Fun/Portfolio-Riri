ec51c06e77b78b984df24607b20feebf29721c35
Riri Feng <danruifun@gmail.com>
2026-04-22 23:11:26 -0400

    update

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,37 +8,40 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi everyone! I’m <span className="purple">Soumyajit Behera</span>{" "}
-            from <span className="purple">Bhubaneswar, India</span>.
+            Hi everyone! I’m <span className="purple">Riri (Feng Danrui)</span>{" "}
+            from <span className="purple">China</span>.
             <br />
             I’m currently working as a{" "}
-            <span className="purple">Software Developer</span> at{" "}
-            <span className="purple">Juspay</span>.
-            <br />I hold an Integrated M.Sc. (IMSc) in{" "}
-            <span className="purple">Mathematics and Computing</span> from{" "}
-            <span className="purple">BIT Mesra</span>.
+            <span className="purple">Marketing Specialist</span>, focusing on B2B marketing, brand promotion, and data analysis.
+            <br />
+            I hold a Bachelor’s degree in{" "}
+            <span className="purple">Advertising</span> from{" "}
+            <span className="purple">Chung-Ang University, South Korea</span>.
+            <br />
+            I’m currently preparing to pursue a{" "}
+            <span className="purple">Master’s in Computer Science</span> at{" "}
+            <span className="purple">Stevens Institute of Technology</span>.
             <br />
             <br />
-            Outside of coding, I love engaging in activities that keep me
-            creative and inspired:
+            Outside of coding, I love engaging in activities that keep me creative and inspired:
           </p>
 
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games 🎮
-            </li>
-            <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs ✍️
+              <ImPointRight /> Working Out & Fitness 💪
             </li>
             <li className="about-activity">
               <ImPointRight /> Traveling and Exploring New Places 🌍
             </li>
+            <li className="about-activity">
+              <ImPointRight /> Watching Movies & Storytelling 🎬
+            </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Keep learning, keep building, and keep exploring."
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Riri</footer>
         </blockquote>
       </Card.Body>
     </Card>
